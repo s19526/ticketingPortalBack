bf1e631009bc5b51ea61035e7e34300111ae0674
s19526 <s19526@pjwstk.edu.pl>
2022-04-15 13:18:27 +0200

    Initial build

---
+++ src/main/java/com/jaguarF/ticketingPortalBack/TicketingPortalBackApplication.java
@@ -0,0 +1,13 @@
+package com.jaguarF.ticketingPortalBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingPortalBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingPortalBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaguarF/ticketingPortalBack/TicketingPortalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaguarF.ticketingPortalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingPortalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
